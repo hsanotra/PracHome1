689c51fdf836d65a77707425f5dad3935531d2ea
hsanotra <sanotrah@sheridancollege.ca>
2020-01-26 18:32:06 -0800

    Practicing push example

--- src/prachomegit/PracHomeGit.java
+++ src/prachomegit/PracHomeGit.java
@@ -18,6 +18,7 @@ public class PracHomeGit {
     {
         // TODO code application logic here
         System.out.println("My name is Honey");
+        System.out.println(2+2);
         
     }
     
